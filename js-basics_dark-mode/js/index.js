bfda880b0f1263096e77134fa0deb4c1543bf944
Ekaterina Chernova <chernova.berlin@gmail.com>
2023-03-17 16:58:01 +0100

    Complete dark mode task

--- js-basics_dark-mode/js/index.js
+++ js-basics_dark-mode/js/index.js
@@ -1,3 +1,18 @@
 console.clear();
 
 const bodyElement = document.querySelector('[data-js="body"]');
+
+const darkButton = document.querySelector('[data-js="dark-mode-button"]');
+darkButton.addEventListener("click", () => {
+  bodyElement.classList.add("button--dark");
+});
+
+const lightButton = document.querySelector('[data-js="light-mode-button"]');
+lightButton.addEventListener("click", () => {
+  bodyElement.classList.remove("button--dark");
+});
+
+const toggleButton = document.querySelector('[data-js="toggle-button"]');
+toggleButton.addEventListener("click", () => {
+  bodyElement.classList.add("button--toggle");
+});
