4a1b2d326da5b5683bcd5ffcb271af8a5ffea2ff
Ekaterina Chernova <chernova.berlin@gmail.com>
2023-03-17 11:46:46 +0100

    Finish social media post challenge

--- js-variables-and-numbers_social-media-post/js/index.js
+++ js-variables-and-numbers_social-media-post/js/index.js
@@ -10,6 +10,12 @@ console.clear();
 */
 
 // --v-- write your code here --v--
+let postTitle = "Instagram Post";
+let postDescription =
+  "This message is an instagram post. I want to share some info with you.";
+let numberOfLikes = 12;
+let userName = "Mary Smith";
+let isReported = true;
 
 // --^-- write your code here --^--
 
@@ -18,5 +24,19 @@ console.clear();
 */
 
 // --v-- write your code here --v--
+console.log(postTitle);
+console.log(postDescription);
+console.log(numberOfLikes);
+console.log(userName);
+console.log(isReported);
+
+numberOfLikes += 1;
+console.log("Number of likes increased by 1 is: ", numberOfLikes);
+
+numberOfLikes += 1;
+console.log("Number of likes increased by 1 is: ", numberOfLikes);
+
+numberOfLikes += 1;
+console.log("Number of likes increased by 1 is: ", numberOfLikes);
 
 // --^-- write your code here --^--
