cadfa88aab77a7c7f1e6f7f0aa53ef9fc596f6ee
Ekaterina Chernova <chernova.berlin@gmail.com>
2023-03-20 22:55:50 +0100

    Finish truthy falsy task

--- js-conditions-and-booleans/challenges/truthy-and-falsy/js/index.js
+++ js-conditions-and-booleans/challenges/truthy-and-falsy/js/index.js
@@ -1,1 +1,57 @@
 //Write here your code.
+
+if (2 == "2") {
+  console.log("true");
+} else {
+  console.log("false");
+}
+if (2 === "2") {
+  console.log("true");
+} else {
+  console.log("false");
+}
+if ("Hello world") {
+  console.log("true");
+} else {
+  console.log("false");
+}
+if ("") {
+  console.log("true");
+} else {
+  console.log("false");
+}
+if (true) {
+  console.log("true");
+} else {
+  console.log("false");
+}
+if (0) {
+  console.log("true");
+} else {
+  console.log("false");
+}
+if (-50) {
+  console.log("true");
+} else {
+  console.log("false");
+}
+if (50) {
+  console.log("true");
+} else {
+  console.log("false");
+}
+if (5 > 1) {
+  console.log("true");
+} else {
+  console.log("false");
+}
+if ("1 > 10") {
+  console.log("true");
+} else {
+  console.log("false");
+}
+if ("false") {
+  console.log("true");
+} else {
+  console.log("false");
+}
