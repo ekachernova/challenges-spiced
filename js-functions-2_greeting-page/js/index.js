e4dd133d37c4f3e5168c804ccb22562e479ce2f0
Ekaterina Chernova <chernova.berlin@gmail.com>
2023-03-22 11:14:13 +0100

    finish greeting page task

--- js-functions-2_greeting-page/js/index.js
+++ js-functions-2_greeting-page/js/index.js
@@ -23,10 +23,28 @@ const display = document.querySelector('[data-js="display"]');
 
 function getGreeting() {
   // Code here
+  const date = new Date().getHours();
+  if (date >= 6 && date <= 12) {
+    return "Good morning";
+  } else if (date >= 13 && date <= 18) {
+    return "Good afternoon";
+  } else if (date >= 19 && date <= 22) {
+    return "Good evening";
+  } else {
+    return "Good night";
+  }
 }
 
 function getDayColor() {
   // Code here
+  const day = new Date().getDay();
+  if (day === "monday") {
+    return "darkgray";
+  } else if (day === "satuday" || day === "sunday") {
+    return "hotpink";
+  } else {
+    return "lightblue";
+  }
 }
 
 display.textContent = getGreeting();
