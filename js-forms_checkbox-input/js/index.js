5986ff8de12f6f23a40e8c307ea54b5a895fa072
Ekaterina Chernova <chernova.berlin@gmail.com>
2023-03-23 22:37:30 +0100

    Finish checkbox task

--- js-forms_checkbox-input/js/index.js
+++ js-forms_checkbox-input/js/index.js
@@ -11,13 +11,42 @@ function showTosError() {
   tosError.removeAttribute("hidden");
 }
 
+function hideDivSucces() {
+  const divSuccesMessage = document.querySelector('[data-js="success"]');
+  divSuccesMessage.setAttribute("hidden", "");
+}
+
+function showDivSucces() {
+  const divSuccesMessage = document.querySelector('[data-js="success"]');
+  divSuccesMessage.removeAttribute("hidden");
+}
+
+//in order to hide error message by reloading the page
+hideTosError();
+hideDivSucces();
+
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   // --v-- write your code here --v--
+  const checkbox = form.tos.checked;
+  if (checkbox === true) {
+    // alert("Form submitted");
+    showDivSucces();
+    divSuccesMessage.textContent = hideTosError();
+    // eslint-disable-next-line no-alert
+  } else {
+    return showTosError();
+  }
 
   // --^-- write your code here --^--
-
-  // eslint-disable-next-line no-alert
-  alert("Form submitted");
+});
+//in order to hide an error message by clicking on checkbox
+tos.addEventListener("change", () => {
+  const checkbox = form.tos.checked;
+  if (checkbox === true) {
+    return hideTosError();
+  } else {
+    hideDivSucces();
+  }
 });
