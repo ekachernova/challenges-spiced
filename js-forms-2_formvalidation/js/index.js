a31cd675c46b30c310a4dd185c027725cfaac2dc
Ekaterina Chernova <chernova.berlin@gmail.com>
2023-03-24 16:55:48 +0100

    Finish the task

--- js-forms-2_formvalidation/js/index.js
+++ js-forms-2_formvalidation/js/index.js
@@ -10,3 +10,13 @@ form.addEventListener("submit", (event) => {
 
   console.log(data);
 });
+
+/**Make sure that the user submits all relevant information in a correct format.
+
+Use the following guidelines:
+
+- First and last name are required and are allowed to have at least 4 and up to 30 characters.
+- The user needs to be at least 14 years old.
+- We need a correct email adress (i.e. including an `@` symbol) for contacting the user.
+- It's necessary that the user has read the terms of service and agrees to them.
+ */
