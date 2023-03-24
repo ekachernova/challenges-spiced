4bc6f87cb80ce3e9abd45298f43c7273f15cb1ea
Ekaterina Chernova <chernova.berlin@gmail.com>
2023-03-24 14:09:50 +0100

    Complete layout of the post

--- js-createelement_social-media-post/js/index.js
+++ js-createelement_social-media-post/js/index.js
@@ -10,3 +10,38 @@ likeButton.addEventListener("click", handleLikeButtonClick);
 
 // Exercise:
 // Use document.createElement() and append another social media post to the body.
+/**
+ * This page shows an example of a social media post. Your task is to replicate the social media post with JavaScript. Use document.createElement() to generate all HTML elements separately.
+
+Don't use .innerHTML to create the HTML in one instance, but use multiple calls of document.createElement() instead.
+
+The index.html demonstrates the required HTML tags, structure and class names.
+
+Append the second social media post to the body. Don't forget to add the event listener to the like button. You can use the function handleLikeButtonClick for the event listener.
+
+ */
+let newArticle = document.createElement("article");
+newArticle.classList.add("post");
+let paragraph = document.createElement("p");
+paragraph.classList.add("post__content");
+paragraph.textContent =
+  "Lorem ipsum dolor, sit amet consectetur adipisicing elit.";
+newArticle.append(paragraph);
+
+let footer = document.createElement("footer");
+// console.log(footer);
+footer.classList.add("post__footer");
+newArticle.append(footer);
+
+let userName = document.createElement("span");
+userName.classList.add("post__username");
+userName.textContent = "@username";
+footer.append(userName);
+
+let button = document.createElement("button");
+button.classList.add("post__button");
+button.setAttribute("type", "button");
+button.innerText = "♥ Like";
+footer.append(button);
+
+document.body.append(newArticle);
