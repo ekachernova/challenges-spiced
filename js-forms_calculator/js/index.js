620551a660e478da00a723b836dd1df7970c9fc6
Ekaterina Chernova <chernova.berlin@gmail.com>
2023-03-24 09:28:55 +0100

    Continue working on the task?

--- js-forms_calculator/js/index.js
+++ js-forms_calculator/js/index.js
@@ -21,12 +21,33 @@ function divide(a, b) {
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-
   let result;
-
   // --v-- write your code here --v--
+  formData = new formData(event.target);
+  data = Object.fromEntries(formData);
 
+  let a = parceInt(data.numberA);
+  let b = parceInt(data.numberB);
+
+  let addition = document.getElementById("addition");
+  let substract = document.getElementById("substraction");
+  let multiplication = document.getElementById("multiplication");
+  let division = document.getElementById("division");
+  if (addition.checked === true) {
+    result = add(a, b);
+    resultOutput.textContent = result;
+  } else if (subtract.checked === true) {
+    result = substract(a, b);
+    resultOutput.textContent = result;
+  } else if (multiplication.checked === true) {
+    result = multiply(a, b);
+    resultOutput.textContent = result;
+  } else if (division.checked === true) {
+    result = divide(a, b);
+    resultOutput.textContent = result;
+  }
   // --^-- write your code here --^--
+  console.log(a + b);
 
   resultOutput.textContent = result;
 });
