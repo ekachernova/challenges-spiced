d092ed62bf7ce757ae7ab6b638827acccd48f298
Ekaterina Chernova <chernova.berlin@gmail.com>
2023-03-25 16:32:35 +0100

    Finish simple live calculator task

--- js-forms-2_simple-live-calculator/js/index.js
+++ js-forms-2_simple-live-calculator/js/index.js
@@ -3,3 +3,25 @@ console.clear();
 const firstInput = document.querySelector('[data-js="firstValue"]');
 const secondInput = document.querySelector('[data-js="secondValue"]');
 const result = document.querySelector('[data-js="result"]');
+
+firstInput.addEventListener("input", (e) => {
+  e.preventDefault();
+  const firstNumber = firstInput.value;
+  const secondNumber = secondInput.value;
+
+  let multiplyNumbers = firstNumber * secondNumber;
+  console.log(multiplyNumbers);
+
+  result.textContent = multiplyNumbers;
+});
+
+secondInput.addEventListener("input", (e) => {
+  e.preventDefault();
+  const firstNumber = firstInput.value;
+  const secondNumber = secondInput.value;
+
+  let multiplyNumbers = firstNumber * secondNumber;
+  console.log(multiplyNumbers);
+
+  result.textContent = multiplyNumbers;
+});
