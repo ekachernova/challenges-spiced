4c036e2cc3afa4726aef3337e9bd895b385620aa
Ekaterina Chernova <chernova.berlin@gmail.com>
2023-03-29 10:23:12 +0200

    Finish task

--- js-callback-functions_user-login-callback-1-named-vs-anonymous/js/index.js
+++ js-callback-functions_user-login-callback-1-named-vs-anonymous/js/index.js
@@ -5,4 +5,24 @@ function handleUserLogin(onSuccess) {
   onSuccess();
 }
 
+function showWelcomeMessage() {
+  console.log("Welcome! You are logged in now.");
+}
 // Call handleUserLogin below!
+
+handleUserLogin(() => {
+  console.log("Welcome! You are logged in now.");
+});
+
+/**You should write a callback function in two different ways.
+In both cases your callback function should log the following text to the console:
+"Welcome! You are logged in now."
+
+1. Write a function with the "function" keyword named "showWelcomeMessage" which logs the desired text. Then
+   call the higher order function "handleUserLogin" and pass your callback function "showWelcomeMessage" as the first argument.
+2. Call "handleUserLogin" again but this time write an anonymous function **inside** the function call (i.e. between the round brackets). You can write an anonymous function either:
+
+   - as an arrow function: ` () => {...}`
+   - as a regular function: ` function() {...}`
+
+3. Implement the logic from the previous from "showWelcomeMessage" inside this anonymous function. */
