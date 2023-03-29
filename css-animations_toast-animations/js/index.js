7e4f526321369e6ce72f05dad3f2759a8629790f
Ekaterina Chernova <chernova.berlin@gmail.com>
2023-03-29 15:08:11 +0200

    Continue workin on the task

--- css-animations_toast-animations/js/index.js
+++ css-animations_toast-animations/js/index.js
@@ -25,3 +25,39 @@ function hideToast() {
   toast.classList.add("toast--hidden");
   countdown.classList.remove("countdown--visible");
 }
+
+/**
+ * ## Task 1
+
+The hover effect on the start button can still be improved. Let's do that.
+
+Find some rules you like and animate them with transitions.
+
+- Use at least one timing-function
+- Assign different durations
+- Consider using delays
+
+> Also check out the handout if you have questions!
+
+## Task 2
+
+When you click the start button, a toast appears in the upper left corner. Currently this toast is animated with transitions.
+
+Make it your own!  
+Here are 3 options to start with:
+
+### Option 1 - Keep Transitions:
+
+Keep animating the toast with transitions.
+
+### Option 2 - Change Transitions to keyframes:
+
+Change the way to animate the toast and use keyframes instead.
+
+> Is much more versatile to customize, but holds far more pitfalls.
+
+### Option 3 - Add your own logic and css:
+
+Do whatever you want. Can you animate the body's background-color while holding your mouse down on the start-button?
+
+ */
