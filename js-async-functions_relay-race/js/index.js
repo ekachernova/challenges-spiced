58650130e98061b8a16e1c34eb586b48df7e330d
Ekaterina Chernova <chernova.berlin@gmail.com>
2023-03-31 11:56:16 +0200

    Finish task

--- js-async-functions_relay-race/js/index.js
+++ js-async-functions_relay-race/js/index.js
@@ -8,7 +8,7 @@ const startRaceButton = document.querySelector('[data-js="start-race-button"]');
 
 // You'll need to change something about this callback function:
 //                                         ↙️
-startRaceButton.addEventListener("click", () => {
+startRaceButton.addEventListener("click", async () => {
   startRaceButton.setAttribute("disabled", "");
 
   /**
@@ -24,6 +24,11 @@ startRaceButton.addEventListener("click", () => {
    **/
 
   // --v-- write your code here --v--
+  // animateRunner(runner1);
+
+  const runOne = await animateRunner(runner1);
+  const runTwo = await animateRunner(runner2);
+  const runThree = await animateRunner(runner3);
 
   // --^-- write your code here --^--
 
