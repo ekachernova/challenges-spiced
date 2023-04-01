86ed187de3edefaa3b0f67b0f9596c7dc5488639
Ekaterina Chernova <chernova.berlin@gmail.com>
2023-04-02 01:35:19 +0200

    Finish task

--- js-array-methods-2_fun-with-flags/index.js
+++ js-array-methods-2_fun-with-flags/index.js
@@ -9,10 +9,34 @@ queryInput.addEventListener("input", (event) => {
 
   const searchString = event.target.value;
 
-  const foundCountry = null;
+  // trying find method
+
+  const foundCountry = countries.find((country) =>
+    country.name.startsWith(searchString)
+  );
+  console.log(foundCountry);
 
   if (foundCountry) {
     const countryElement = Country(foundCountry);
     container.append(countryElement);
   }
+
+  // trying filter method
+
+  const foundCountries = countries.filter((country) =>
+    country.name.startsWith(searchString)
+  );
+  console.log(foundCountries);
+
+  foundCountries.forEach((country) => {
+    const countryElement = Country(foundCountries);
+    container.append(countryElement);
+  });
 });
+
+/**
+
+Change the method from `find` to `filter`, to filter the countries to those which name starts with the search string. Remember to change the variable name from `foundCountry` to `foundCountries` to reflect that it now is an array.
+
+Adapt the rendering of the found countries. You'll need to loop over the array of `foundCountries`. Which array method can you use for that?
+ */
