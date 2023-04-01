6044839ac51ef1ebc35275346ff817303cd1ba9f
Ekaterina Chernova <chernova.berlin@gmail.com>
2023-04-02 00:45:43 +0200

    Finish task

--- js-array-methods-2_news-flash/index.js
+++ js-array-methods-2_news-flash/index.js
@@ -5,9 +5,11 @@ import { checkFilteredNews, checkSortedNews } from "./utils/results.js";
 const container = document.querySelector('[data-js="card-container"]');
 
 // Part 1 - start here
-const filteredNews = news.filter(() => {
-  return true;
-});
+const filteredNews = news.filter((card) =>
+  card.categories.includes("politics")
+);
+
+console.log("article politics ", filteredNews);
 
 // Part 2 - start here
 const sortedNews = filteredNews;
@@ -15,6 +17,17 @@ const sortedNews = filteredNews;
 sortedNews.forEach((news) => {
   const cardElement = Card(news);
   container.append(cardElement);
+});
+
+sortedNews.sort((a, b) => {
+  const lengthA = a.body.length;
+  const lengthB = b.body.length;
+  if (lengthA > lengthB) {
+    return 1;
+  } else if (lengthA < lengthB) {
+    return -1;
+  }
+  return 0;
 });
 
 // Check your filter and sorting order here:
