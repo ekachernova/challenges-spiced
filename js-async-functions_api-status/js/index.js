45976e0f055206342b855a3a848b578d91ebb28d
Ekaterina Chernova <chernova.berlin@gmail.com>
2023-04-02 23:02:31 +0200

    Finish task

--- js-async-functions_api-status/js/index.js
+++ js-async-functions_api-status/js/index.js
@@ -4,51 +4,24 @@ const button = document.querySelector('[data-js="status-button"]');
 const apiStatusUrl = "https://example-apis.vercel.app/api/status";
 
 // Something needs to change here…
-// async function postData(url = "", data = {}) {
-async function checkIfApiIsAvailable(apiStatusUrl) {
-  try {const response = await fetch(apiStatusUrl);
-  if (response.ok) {
-    icon.textContent
-  }}
+async function checkIfApiIsAvailable() {
+  try {
+    icon.textContent = "⏳";
+    const response = await fetch(apiStatusUrl);
 
-  /**
-   * Hint 1:
-   * Use the `fetch()` function and pass the `apiStatusUrl` into it
-   *
-   * Hint 2:
-   * The fetch function returns a promise which resolves to a Response
-   * object once it is ready.
-   *
-   * Hint 3:
-   * The Response object has a `ok` property which is true if the response
-   * is okay and false if it is not.
-   **/
-  // --v-- write your code here --v--
-  // --^-- write your code here --^--
-
-  return response;
+    if (!response.ok) {
+      console.log("error");
+      icon.textContent = "❌";
+    } else {
+      console.log("yes");
+      icon.textContent = "✅";
+    }
+  } catch (error) {
+    console.log(`Error message: ${response.message}`);
+    icon.textContent = "❌";
+  }
 }
 
-button.addEventListener("click", () => {
+button.addEventListener("click", (event) => {
   checkIfApiIsAvailable();
 });
-
-/**
- There is a function called `checkIfAPIIsAvailable`; inside of this function, the `response` variable is declared with the result of `fetch(api)`.
-
-The function is called everytime you click the button.
-
-Your task is to refactor `checkIfAPIIsAvailable` so that - depending on `response.ok` - the icon displays either ✅ or ❌.
-
-You can use the following hints as guideline:
-
-- `fetch()` returns a promise, so make sure to wait for the result.
-- Check whether 'response.ok' is truthy;
-  - if so, the icon should display ✅
-  - if not, the icon should display ❌
-- Use a `try...catch` block.
-
-## Bonus:
-
-Can you display a '⏳' icon while waiting for the response?
- */
