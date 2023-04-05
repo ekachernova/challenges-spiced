1fff01e12dd76ec1cccbb5c50724a86560915d0d
Ekaterina Chernova <zvirbulyonok@gmail.com>
2023-04-05 10:36:34 +0200

    Add description to index.js

--- js-modern-syntax_destructuring-objects/index.js
+++ js-modern-syntax_destructuring-objects/index.js
@@ -40,3 +40,15 @@ const person = {
   age: 24,
   firstName: "Alex",
 };
+
+/**In this Codesandbox, you will destructure objects.
+
+
+## Hints
+
+- Use the variable names given in the task to destructure the given object.
+- The value can correspond to a simple data type as well as to an object.
+- Make sure to export the variable you have declared in order to make the tests work.
+- If you want to check whether you solved the tasks correctly, open the "Tests" tab of Codesandbox.
+
+ */
