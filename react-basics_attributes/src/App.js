0796f52fba8c172fa1d996434b2cede4d2c40b92
Ekaterina Chernova <zvirbulyonok@gmail.com>
2023-04-11 16:05:45 +0200

    finish task

--- react-basics_attributes/src/App.js
+++ react-basics_attributes/src/App.js
@@ -2,5 +2,16 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <div>Replace me with an article</div>;
+  return <Article/>;
+}
+
+function Article() {
+  return (<article className='article__title'>
+    <label htmlFor="input" >This is input</label>
+    <input id="input" name="name"></input>
+    <a className ='article__link' href="https://en.wkipedia.org/wiki/Vincent_van_Gogh">Whould you like to know something about Van Gogh?
+      </a>
+
+  </article>
+  )
 }
