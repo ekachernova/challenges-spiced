849a112053825b14a038ee821394145fec2105d5
Ekaterina Chernova <zvirbulyonok@gmail.com>
2023-04-14 11:02:57 +0200

    finish render fruits

--- react-with-arrays_fruits/src/App.js
+++ react-with-arrays_fruits/src/App.js
@@ -1,11 +1,48 @@
 import Card from "./components/Card";
 
 export default function App() {
-  const fruits = [];
+  //Done: Create an array 'fruits'. It should contain at least 5 objects which all have the properties 'name', 'id', 'color'
+
+  const fruits = [
+    {
+      id: 1337,
+      name: '🍌 Banana',
+      color: 'yellow',
+    },
+    {
+      id: 1338,
+      name: '🍊 Orange',
+      color: 'orange',
+    },
+    {
+      id: 1339,
+      name: '🥑 Avocado',
+      color: 'green',
+    },
+    {
+      id: 1340,
+      name: '🍒 Cherry',
+      color: 'red',
+    },
+    {
+      id: 1341,
+      name: '🥝 Kiwi',
+      color: 'green',
+    },
+    {
+      id: 1342,
+      name: '🥥 Cocos',
+      color: 'white',
+    }
+  ];
+
+
 
   return (
     <div className="app">
-      <Card name="🍌 banana" />
+      {fruits.map(({ id, name}) => (
+      <Card key = {id} name= {name}/>
+      ))}
     </div>
   );
 }
