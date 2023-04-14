af4362ad365376d63f4371218c660383fbb9aa9c
Ekaterina Chernova <zvirbulyonok@gmail.com>
2023-04-14 11:05:50 +0200

    finish task

--- react-with-arrays_fruits/src/App.js
+++ react-with-arrays_fruits/src/App.js
@@ -40,8 +40,8 @@ export default function App() {
 
   return (
     <div className="app">
-      {fruits.map(({ id, name}) => (
-      <Card key = {id} name= {name}/>
+      {fruits.map(({ id, name, color}) => (
+      <Card key = {id} name= {name} backgroundColor = {color}/>
       ))}
     </div>
   );
