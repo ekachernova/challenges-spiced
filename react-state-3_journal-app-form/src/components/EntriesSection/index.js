1a63d73f03093ee27340217c0062c3e3a87f0ebd
Ekaterina Chernova <zvirbulyonok@gmail.com>
2023-04-17 16:05:48 +0200

    finish task

--- react-state-3_journal-app-form/src/components/EntriesSection/index.js
+++ react-state-3_journal-app-form/src/components/EntriesSection/index.js
@@ -6,37 +6,44 @@ import Tab from "../Tab";
 import Badge from "../Badge";
 import { Fragment } from "react";
 
-const entries = [
-  {
-    id: 1000,
-    date: "Feb 5, 2025",
-    motto: "We are in a state of chaos",
-    notes:
-      "Today I learned about React State. It was fun! I can't wait to learn more.",
-  },
-  {
-    id: 999,
-    date: "Feb 4, 2025",
-    motto: "Props, Props, Props",
-    notes:
-      "Today I learned about React Props. Mad props to everyone who understands this!",
-  },
-  {
-    id: 998,
-    date: "Feb 3, 2025",
-    motto: "How to nest components online fast",
-    notes:
-      "Today I learned about React Components and how to nest them like a pro. Application design is so much fun!",
-  },
-  {
-    id: 997,
-    date: "Feb 2, 2025",
-    motto: "I'm a React Developer",
-    notes: "My React-ion when I learned about React: Yay!",
-  },
-];
 
-export default function EntriesSection() {
+// //done: rename entries to initialEntries for useState 
+// const initialEntries = [
+//   {
+//     id: 1000,
+//     date: "Feb 5, 2025",
+//     motto: "We are in a state of chaos",
+//     notes:
+//       "Today I learned about React State. It was fun! I can't wait to learn more.",
+//   },
+//   {
+//     id: 999,
+//     date: "Feb 4, 2025",
+//     motto: "Props, Props, Props",
+//     notes:
+//       "Today I learned about React Props. Mad props to everyone who understands this!",
+//   },
+//   {
+//     id: 998,
+//     date: "Feb 3, 2025",
+//     motto: "How to nest components online fast",
+//     notes:
+//       "Today I learned about React Components and how to nest them like a pro. Application design is so much fun!",
+//   },
+//   {
+//     id: 997,
+//     date: "Feb 2, 2025",
+//     motto: "I'm a React Developer",
+//     notes: "My React-ion when I learned about React: Yay!",
+//   },
+// ];
+
+export default function EntriesSection({entries}) {
+
+  //   //etries should be stored in the initial state in order to be able to update this state later
+  // const [entries,setEntries] = useState(initialEntries)
+ 
+
   return (
     <section className="entries-section">
       <Tabs>
