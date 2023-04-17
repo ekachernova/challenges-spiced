2ef491ea2ec595913e4bb40df8ae2563ad3a7eaa
Ekaterina Chernova <zvirbulyonok@gmail.com>
2023-04-17 23:28:39 +0200

    finish task

--- react-effects-and-fetch_iss-tracker/src/App.js
+++ react-effects-and-fetch_iss-tracker/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Controls from "./components/Controls";
 import Map from "./components/Map";
 import "./styles.css";
@@ -11,7 +11,34 @@ export default function App() {
     latitude: 0,
   });
 
-  async function getISSCoords() {}
+  async function getISSCoords() {
+    try {
+    const response = await fetch(URL);
+    const data = await response.json();
+    console.log(data);
+    if (data.latitude && data.longitude) {
+    setCoords({
+      longitude:data.longitude,
+      latitude:data.latitude,
+    });
+  }
+    }
+    catch (e) {
+      console.error(e)
+    }
+
+  }
+
+  useEffect(() => {
+    const interval = setInterval(() =>{
+      getISSCoords();
+    },5000)
+    //clean-up function
+    return () => {
+      clearInterval(interval)
+    }
+  }
+  )
 
   return (
     <main>
