77b5ded09d955749280f91299d6e6efe1f73c314
Ekaterina Chernova <chernova.berlin@gmail.com>
2023-04-24 15:56:52 +0200

    finish label task

--- react-component-testing_scorekeeper/components/Input/Input.test.js
+++ react-component-testing_scorekeeper/components/Input/Input.test.js
@@ -2,6 +2,12 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import Input from ".";
 
-test("renders a label and an input with the correct attributes", () => {});
+test("renders a label and an input with the correct attributes", () => {
+  render(<Input labelText="Name" placeholder="placeholder" name="name" />);
+  const inputToCheck = screen.getByLabelText("Name");
+
+  expect(inputToCheck).toHaveAttribute("name", "name");
+  expect(inputToCheck).toHaveAttribute("placeholder", "placeholder");
+});
 
 test("calls callback on every user input", async () => {});
