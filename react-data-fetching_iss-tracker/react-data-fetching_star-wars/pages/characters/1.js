7099a63f5258cbab279e7e5e203e5b6988caebac
Ekaterina Chernova <chernova.berlin@gmail.com>
2023-04-26 14:17:03 +0200

    finish fetch data

--- react-data-fetching_iss-tracker/react-data-fetching_star-wars/pages/characters/1.js
+++ react-data-fetching_iss-tracker/react-data-fetching_star-wars/pages/characters/1.js
@@ -1,17 +1,32 @@
+import { useState } from "react";
 import Card from "../../components/Card";
 import Layout from "../../components/Layout";
+import useSWR from "swr";
+import { useRouter } from "next/router";
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 export default function Character() {
-  const id = 1;
+  const router = useRouter();
+  const { id } = router.query;
+
+  const { data, error, isLoading } = useSWR(
+    `https://swapi.dev/api/people/?${id}`,
+    fetcher
+  );
+  console.log("data", data);
+
+  if (error) return <p>Error..</p>;
+  if (isLoading) return <p>Loading...</p>;
 
   return (
     <Layout>
       <Card
-        id={id}
-        name={"Luke Skywalker"}
-        height={172}
-        eyeColor={"blue"}
-        birthYear={"19BBY"}
+        id={data.id}
+        name={data.name}
+        height={data.height}
+        eyeColor={data.eye_olor}
+        birthYear={data.birth_year}
       />
     </Layout>
   );
