a14d666c04b002c5f5ac2861cb53a24284e676dd
Ekaterina Chernova <chernova.berlin@gmail.com>
2023-05-04 12:21:42 +0200

    finish task

--- backend-basics_chance/index.js
+++ backend-basics_chance/index.js
@@ -1,3 +1,7 @@
-// import { server } from "./server.js";
+import { server } from "./server.js";
 
-// server.listen(…
+const port = 7999;
+server.listen(port, () => {
+  console.log(`Server running at http://127.0.0.1:${port}/`);
+  console.log("hi");
+});
