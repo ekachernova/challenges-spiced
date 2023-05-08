acb9c1bac8b5cf06ed0c243c48e6d9f0d9f4e54f
Ekaterina Chernova <chernova.berlin@gmail.com>
2023-05-08 16:54:20 +0200

    Animation branch

---
+++ backend-create_products/components/ProductList/index.js
@@ -0,0 +1,33 @@
+import useSWR from "swr";
+import { useRouter } from "next/router";
+import { StyledHeading, StyledList } from "./ProductList.styled";
+import { StyledButton } from "../Button/Button.styled";
+
+export default function ProductList() {
+  const router = useRouter();
+  const { data } = useSWR("/api/products");
+
+  console.log("data: ", data);
+
+  if (!data) {
+    return <h1>Loading...</h1>;
+  }
+
+  return (
+    <>
+      <StyledHeading>Available Fishes</StyledHeading>
+      <StyledList>
+        {data.map((product) => (
+          <li key={product._id}>
+            <StyledButton
+              type="button"
+              onClick={() => router.push(`/${product._id}`)}
+            >
+              {product.name}
+            </StyledButton>
+          </li>
+        ))}
+      </StyledList>
+    </>
+  );
+}
